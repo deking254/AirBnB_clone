11721c5add37e882d1702bb7fcc83a4e417a04b9
abraham-ship <bettabraham11@gmail.com>
2023-07-13 02:38:18 -0700

    updated model

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """base for all other classes"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -19,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         """string representation of object"""
@@ -30,6 +32,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
