11c7a83598f91e6cc31c08938ec93501b470f070
abraham-ship <bettabraham11@gmail.com>
2023-07-13 02:27:25 -0700

    storing objects

---
+++ models/engine/file_storage.py
@@ -0,0 +1,46 @@
+#!usr/bin/python3
+
+"""define class FileStorage"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    Public class attribute of class FileStorage
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
+        for key, value in self.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects (only if the JSON file
+        (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    class_obj = eval(class_name)
+                    self.__objects[key] = class_obj(**value)
+        except FileNotFoundError:
+            return
