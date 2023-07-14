197d129a2bc626bd3ddb28ae3d5a7fcdd4363dd7
deking254 <dekingsky@gmail.com>
2023-07-14 09:23:12 +0000

    update on the console.py

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 import cmd
 import json
 from models.base_model import BaseModel
+from models import storage
 class HBNBCommand(cmd.Cmd):
     cmd.Cmd.prompt = '(hbnb) '
     def do_quit(self, arg):
@@ -14,13 +15,201 @@ class HBNBCommand(cmd.Cmd):
         """Creates a new instance of BaseModel"""
         if arg: 
             if arg == 'BaseModel':
-                newinst = BaseModel() 
-                with open("file.json", 'w+') as fd:
-                    print(json.dumps(newinst.to_dict()))
-                print(newinst.id)
+                new = BaseModel()
+                new.save()
+                print(new.id)
             else:
                 print("** class doesn't exist **")
         else:
             print("** class name missing **")
+    def check_args(self, arg):
+        """
+            a function to tokenize the arguments
+        """
+        first = ""
+        status = 0
+        args = []
+        for i in arg:
+            if i == '"':
+                status += 1
+                continue
+            if i == " ":
+                if status == 1:
+                    first += i
+                elif status == 2:
+                    args.append(first)
+                    first = ""
+                else:
+                    args.append(first)
+                    first = ""
+            else:
+                if i == '"':
+                    args.append(first)
+                    first = ""
+                else:
+                    first += i
+        args.append(first)
+        return (args)
+    def do_show(self, arg=None):
+        """
+            Prints the string representation of an instance
+        """
+        status = 0
+        instance_present = 0
+        if arg == None:
+            print("** class name missing **")
+            return(0)
+        else:
+            arguments = self.check_args(arg)
+            classnames = []
+            instances = storage.all()
+            for key in instances.keys():
+                word = ""
+                for letter in key:
+                    if letter == ".":
+                        classnames.append(word)
+                        break
+                    else:
+                        word += letter
+            for name in classnames:
+                if name == arguments[0]:
+                    status = 1
+            if status == 0:
+                print("** class doesn't exist **")
+            else:
+                if len(arguments) >= 2:
+                    classname = arguments[0] + "." + arguments[1]
+                    for classandid in instances.keys():
+                        if classname == classandid:
+                            instance_present = 1
+                            print(instances.get(classname).__str__())
+                    if instance_present == 0:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+    def do_destroy(self, arg=None):
+        """
+            Deletes an instance based on the class name and id
+        """
+        status = 0
+        instance_present = 0
+        if arg == None:
+            print("** class name missing **")
+            return(0)
+        else:
+            arguments = self.check_args(arg)
+            classnames = []
+            instances = storage.all()
+            for key in instances.keys():
+                word = ""
+                for letter in key:
+                    if letter == ".":
+                        classnames.append(word)
+                        break
+                    else:
+                        word += letter
+            for name in classnames:
+                if name == arguments[0]:
+                    status = 1
+            if status == 0:
+                print("** class doesn't exist **")
+            else:
+                if len(arguments) >= 2:
+                    classname = arguments[0] + "." + arguments[1]
+                    for classandid in instances.keys():
+                        if classname == classandid:
+                            instance_present = 1
+                            instances.pop(classname)
+                            storage.save()
+                            break
+                    if instance_present == 0:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+
+    def do_all(self, args):
+        """
+            Prints all string representation of all instances
+        """
+        instances = storage.all()
+        instances_array = []
+        status = 0
+        if args:
+            arguments = self.check_args(args)
+            instances = storage.all()
+            for key in instances.keys():
+                word = ""
+                for letter in key:
+                    if letter == ".":
+                        if word == arguments[0]:
+                            status = 1
+                            instances_array.append(instances.get(key).__str__())
+                        break
+                    else:
+                        word += letter
+            if status == 0:
+                print("** class doesn't exist **")
+            else:
+                print(instances_array)
+        else:
+            for value in instances.values():
+                instances_array.append(value.__str__())
+            print(instances_array)
+    def do_update(self, arg):
+        """
+            Updates an instance based on the class name and id
+        """
+        status = 0
+        instance_present = 0
+        if arg == None:
+            print("** class name missing **")
+            return(0)
+        else:
+            arguments = self.check_args(arg)
+            classnames = []
+            instances = storage.all()
+            for key in instances.keys():
+                word = ""
+                for letter in key:
+                    if letter == ".":
+                        classnames.append(word)
+                        break
+                    else:
+                        word += letter
+            for name in classnames:
+                if name == arguments[0]:
+                    status = 1
+            if status == 0:
+                print("** class doesn't exist **")
+            else:
+                if len(arguments) >= 2:
+                    classname = arguments[0] + "." + arguments[1]
+                    for classandid in instances.keys():
+                        if classname == classandid:
+                            if len(arguments) == 2:
+                                print("** attribute name missing **")
+                                return
+                            else:
+                                if len(arguments) ==3:
+                                    print("** value missing **")
+                                    return
+                                else:
+                                    instance_present = 1
+                                    instance_dict = instances.get(classname).to_dict()
+                                    if instance_dict.get(arguments[2]):
+                                        typeof = type(instance_dict.get(arguments[2]))
+                                        instance_dict.update({arguments[2]:(typeof)(arguments[3])})
+                                        obj = BaseModel(**instance_dict)
+                                    else:
+                                        instance_dict.update({arguments[2]:arguments[3]})
+                                        obj = BaseModel(**instance_dict)
+                                    storage.new(obj)
+                                    storage.save()
+                                    break
+                    if instance_present == 0:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")  
+
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+        HBNBCommand().cmdloop()
